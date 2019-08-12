f81a7d2b05042382751e5457091906dbd385d54d
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-08-12 20:50:35 +0300

    some routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { RouterModule, Routes } from '@angular/router';
+import { VladComponent } from './vlad/vlad.component';
+import { AlexComponent } from './alex/alex.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VladComponent,
+    AlexComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreRouterConnectingModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
